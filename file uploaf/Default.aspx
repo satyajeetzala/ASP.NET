54bbdbd9cbd4800ae3c1533854d1b1793a2e9a31
Satyajeet <szala255@gmail.com>
2024-02-14 13:19:20 +0530

    file upload

---
+++ file uploaf/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Upload File" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="UploadFile" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Upload a File
+    </h2>
+    <asp:FileUpload ID="FileUploadControl" runat="server" />
+    <asp:Button ID="UploadButton" runat="server" Text="Upload File" OnClick="UploadButton_Click" />
+    <asp:Label ID="StatusLabel" runat="server" Text=""></asp:Label>
+</asp:Content>
---
+++ file uploaf/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+public partial class UploadFile : System.Web.UI.Page
+{
+    protected void UploadButton_Click(object sender, EventArgs e)
+    {
+        if (FileUploadControl.HasFile)
+        {
+            try
+            {
+                string filename = Path.GetFileName(FileUploadControl.FileName);
+                FileUploadControl.SaveAs(Server.MapPath("~/Styles/") + filename);
+                StatusLabel.Text = "Upload status: File uploaded!";
+            }
+            catch (Exception ex)
+            {
+                StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occurred: " + ex.Message;
+            }
+        }
+        else
+        {
+            StatusLabel.Text = "Upload status: Please select a file to upload.";
+        }
+    }
+}
