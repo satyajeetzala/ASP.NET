949cfb9168f2367e417215be1f42e8ef385f18d3
Satyajeet <szala255@gmail.com>
2024-03-09 09:00:33 +0530

    new websites

---
+++ traffic lights/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+   <center>
+
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+    <asp:Timer ID="Timer1" runat="server" Interval="1000" ontick="Timer1_Tick">
+    </asp:Timer>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    <br />
+    <br />
+    <asp:Image ID="Image2" runat="server" Height="282px" Width="108px" />
+
+</center>
+</asp:Content>
---
+++ traffic lights/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+      
+    }
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        if (Timer1.Interval == 1000)
+        {
+            Label2.Text = Timer1.Interval.ToString();
+            Image2.ImageUrl = "img/RedLight.png";
+            Timer1.Interval = 6000;
+        }
+        else if (Timer1.Interval == 6000)
+        {
+            Label2.Text = Timer1.Interval.ToString();
+            Image2.ImageUrl = "img/GreenLight.png";
+            Timer1.Interval = 4000;
+        }
+        else if (Timer1.Interval == 4000)
+        {
+            Label2.Text = Timer1.Interval.ToString();
+            Image2.ImageUrl = "img/YellowLight.png";
+            Timer1.Interval = 1000;
+        }
+    }
+}
